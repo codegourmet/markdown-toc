ee2abb01994414319bf5b8fb1dc05d33dd70c13d
Markus Seeger <mail@codegourmet.de>
2014-04-04 16:52:38 +0200

    used optparser

--- markdown-toc.rb
+++ markdown-toc.rb
@@ -8,6 +8,7 @@ THIS_DIR = File.dirname(THIS_FILE)
 require File.join(THIS_DIR, 'toc_tracker.rb')
 require File.join(THIS_DIR, 'toc_node.rb')
 
+require 'optparse'
 require 'tempfile'
 
 TOC_INDENT = 4
@@ -24,14 +25,49 @@ TOC_START_MARKER = "[//]: # (TOC)"
 TOC_END_MARKER = "[//]: # (/TOC)"
 
 def parse_options(args)
-  if ARGV.count == 0
-    raise ArgumentError.new("no input file supplied")
+  options = {}
+
+  option_parser = OptionParser.new do |parser|
+    parser.banner = "Usage: markdown-toc.rb [options] input.md [output.md]"
+    parser.separator ""
+    parser.separator "Specific options:"
+
+    parser.on("-s", "--strip", "remove generated TOC content") do
+      options[:strip] = true
+    end
+
+    parser.on("-i", "--ignore-root", "ignore root heading (e.g. project name)") do
+      options[:ignore_root] = true
+    end
+
+    parser.on("-r", "--replace", "replace input file") do
+      options[:replace] = true
+    end
+
+
+    parser.separator ""
+    parser.separator "Common options:"
+
+    parser.on_tail("-h", "--help", "Show this message") do
+      puts "\n#{parser}\n"
+      exit
+    end
   end
 
-  options = {infile: ARGV[0]}
+  option_parser.parse!
+
+  if ARGV.count == 0
+    puts "\n#{option_parser}\n"
+    exit
+  end
+
+  options[:infile] = ARGV[0]
 
   if ARGV.count == 2
     options[:outfile] = ARGV[1]
+    if options[:replace] == true
+      raise ArgumentError.new("cannot combine '-r' option with outfile parameter")
+    end
   end
 
   options
